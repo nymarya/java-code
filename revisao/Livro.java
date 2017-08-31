e7abf7d1042091460a4fe5b82be5f455ebc0e8c6
Mayra Azevedo <mayradazevedo@gmail.com>
2017-08-31 10:01:24 -0300

    revisao

---
+++ revisao/Livro.java
@@ -0,0 +1,26 @@
+public class Livro {
+    
+    private String titulo;
+    private String genero;
+    private String autor;
+    private int isbn;
+    private int anoPublicacao;
+
+    /**
+     * Construtor para classe Livro.
+     * @param titulo Titulo do livro.
+     * @param genero Gênero do livro.
+     * @param autor Autor do livro.
+     * @param isbn ISBN do livro.
+     * @param anoPublicacao Ano de publicação do livro.
+     */
+    public Livro( String titulo, String genero, String autor, int isbn, int anoPublicacao) {
+	this.titulo = titulo;
+	this.genero = genero;
+	this.autor = autor;
+	this.isbn = isbn;
+	this.anoPublicacao;
+    }
+
+
+}
